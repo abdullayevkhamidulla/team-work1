48e4068ece36c4b5f6a2a4e05127b8d2c3f30db5
ramil77777777777 <irsmanbetovramil1@gmail.com>
2025-03-13 15:23:16 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
   return (
     <>
       <h1>Hello team work</h1>
-      <h2>hello world</h2>
+        <h2>hello world</h2>
       <h3>hello third commit</h3>
       <h4>Ramil comitted</h4>
     </>
-  );``
+  );
 }
 
 export default App;
